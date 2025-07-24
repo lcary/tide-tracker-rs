881f8162925383599377e25b170c750ee457a691
chancelloradams <6766704+lcary@users.noreply.github.com>
2025-07-24 12:42:42 -0400

    Update get-binary.sh

--- scripts/get-binary.sh
+++ scripts/get-binary.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Fetches the release binary for the tide tracker project
-curl -L https://github.com/lcary/tide-tracker-rs/releases/download/v0.1/tide-tracker > tide-tracker
+curl -L https://github.com/lcary/tide-tracker-rs/releases/download/v0.2/tide-tracker > tide-tracker
 chmod +x ./tide-tracker
